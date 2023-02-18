23bd7cf458b91d617d2ed7151cbe995d7445af3b
Orkhan <orkhanmirzayev96@gmail.com>
2023-02-18 17:13:21 +0100

    rules-implemented

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .querySelector(".rules-button")
+    .addEventListener("click", () => open_rules());
+
+  document
+    .querySelector(".cross-img")
+    .addEventListener("click", () => close_rules());
+
+  document
+    .querySelector(".bg-model")
+    .addEventListener("click", () => close_rules());
+});
+
+function open_rules() {
+  document.querySelector(".bg-model").style.display = "flex";
+}
+
+function close_rules() {
+  document.querySelector(".bg-model").style.display = "none";
+}
